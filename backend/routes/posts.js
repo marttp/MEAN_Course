acebd806c6eee72a2ee5f28115c10b0e18e92957
Thanaphoom Babparn <mart-tp@hotmail.com>
2018-07-25 18:14:16 +0700

    Upload image to Webserver and update response data with image path

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,118 +4,157 @@ const router = express.Router();
 
 const Post = require('../models/post.model');
 
+const multer = require('multer');
 
-router.post('',(req,res,next) => {
-    // const post = req.body;
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    })
-    console.log(post);
-    post.save()
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+  // js object for diskStorage have 2 keys : destination and filename
+  // execute when multer try to save a file
+  destination: (req, file, callback) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type');
+    if(isValid){
+        error = null;
+    }
+    callback(error, "backend/images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    // add extension for check filename
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    callback(null, name + "-" + Date.now() + '.' + ext);
+  }
+});
+
+// throw middleware between them
+router.post('', multer({storage: storage}).single('image'), (req, res, next) => {
+  // const post = req.body;
+  const url = req.protocol + '://' + req.get('host');
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: url + '/images/' + req.file.filename
+  })
+  console.log(post);
+  post.save()
     .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message:'Post added successfully',
-            postId: result._id
-        });
-    })
-    .catch(err => {
-        res.status(400).json({
-            message:'Add unsuccessful'
-        });
-    })
-
-});
-
-router.get('',(req,res,next) => {
-    //return normal
-    // res.send('Hello from express');
-
-    //return json data
-
-
-    // const posts = [
-    //     {
-    //         id:'test12345',
-    //         title:'First server-side-post',
-    //         content: 'This is coming from the server 1!'
-    //     },
-    //     {
-    //         id:'test22345',
-    //         title:'Second server-side-post',
-    //         content: 'This is coming from the server 2!'
-    //     },
-    //     {
-    //         id:'test32345',
-    //         title:'Third server-side-post',
-    //         content: 'This is coming from the server 3!'
-    //     }
-    // ];
-
-
-
-    //just pass the json
-    // res.json(posts);
-
-    Post.find().exec()
-    .then(documents => {
-        console.log(documents);
-        // const posts = documents;
-        res.status(200).json({
-            message: 'Posts fetched succesfully!',
-            posts: documents
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json({
-            message: 'Error for execute posts!',
-        });
-    })
-    //pass modify type of json
-
-});
-
-
-router.get('/:id',(req,res,next) => {
-    Post.findById(req.params.id)
-    .then( post => {
-       if(post){
-            res.status(200).json(post);
-       } else {
-            res.status(400).json({
-                message: 'Post not found!',
-            });       
+      console.log(result);
+      res.status(201).json({
+        message: 'Post added successfully',
+        post: {
+            // ...result,
+            id: result._id,
+            title: result.title,
+            content: result.content,
+            imagePath: result.imagePath
         }
+      });
+    })
+    .catch(err => {
+      res.status(400).json({
+        message: 'Add unsuccessful'
+      });
+    })
+
+});
+
+router.get('', (req, res, next) => {
+  //return normal
+  // res.send('Hello from express');
+
+  //return json data
+
+
+  // const posts = [
+  //     {
+  //         id:'test12345',
+  //         title:'First server-side-post',
+  //         content: 'This is coming from the server 1!'
+  //     },
+  //     {
+  //         id:'test22345',
+  //         title:'Second server-side-post',
+  //         content: 'This is coming from the server 2!'
+  //     },
+  //     {
+  //         id:'test32345',
+  //         title:'Third server-side-post',
+  //         content: 'This is coming from the server 3!'
+  //     }
+  // ];
+
+
+
+  //just pass the json
+  // res.json(posts);
+
+  Post.find().exec()
+    .then(documents => {
+      console.log(documents);
+      // const posts = documents;
+      res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: documents
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({
+        message: 'Error for execute posts!',
+      });
+    })
+  //pass modify type of json
+
+});
+
+
+router.get('/:id', (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(400).json({
+          message: 'Post not found!',
+        });
+      }
     })
 
 })
 
-router.put('/:id',(req,res,next) => {
+router.put('/:id', (req, res, next) => {
 
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    })
-    Post.updateOne({ _id: req.params.id }, post)
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  })
+  Post.updateOne({
+      _id: req.params.id
+    }, post)
     .then(result => {
-        res.status(200).json({
-            message: "Update successful!"
-        });
+      res.status(200).json({
+        message: "Update successful!"
+      });
     })
 });
 
 
-router.delete('/:id',(req,res,next)=>{
-    // console.log(req.params.id);
-    Post.deleteOne({ _id: req.params.id })
+router.delete('/:id', (req, res, next) => {
+  // console.log(req.params.id);
+  Post.deleteOne({
+      _id: req.params.id
+    })
     .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: 'Post deleted'
-        });
+      console.log(result);
+      res.status(200).json({
+        message: 'Post deleted'
+      });
     });
 
 })
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -45,20 +45,38 @@ export class PostsService {
     }
 
     getPost(id: String){
-        return this.http.get<{_id: string, title: string, content: string }>
+        return this.http.get<{
+            _id: string,
+            title: string,
+            content: string
+        }>
         ('http://localhost:3000/api/posts/' + id);
     }
 
-    addPost(title: String, content: String) {
-        const post: Post = {
-            id: null,
-            title: title,
-            content: content};
-        this.http.post<{message: string, postId: String}>('http://localhost:3000/api/posts', post)
+    addPost(title: string, content: string, image: File) {
+        // const post: Post = {
+        //     id: null,
+        //     title: title,
+        //     content: content};
+
+        /*
+            Change it to FormData
+         */
+        const postData = new FormData();
+        postData.append('title', title);
+        postData.append('content', content);
+        postData.append('image', image, title);
+        this.http.post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
         .subscribe((responseData) => {
+            const post: Post = {
+                id: responseData.post.id,
+                title: title,
+                content: content,
+                imagePath: responseData.post.imagePath
+            };
             // console.log(responseData.message);
-            const postId = responseData.postId;
-            post.id = postId;
+            // const postId = responseData.postId;
+            // post.id = postId;
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
             this.router.navigate(['/']);
@@ -67,7 +85,7 @@ export class PostsService {
 
 
     updatePost(id: String, title: String, content: String){
-        const post: Post = { id: id, title: title, content: content };
+        const post: Post = { id: id, title: title, content: content, imagePath: null };
         this.http.put('http://localhost:3000/api/posts/' + id, post)
         .subscribe((response) => {
             const updatedPosts = [...this.posts];
